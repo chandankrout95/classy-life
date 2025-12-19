6ac50b224b398a0e4b2279f67f1ff025e33d8736
dipesh Dhillon <dipeshdhillon2006@gmail.com>
2025-12-19 19:53:14 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,9 @@ export function cn(...inputs: ClassValue[]) {
 
 
 export function formatNumber(num: number): string {
+  if (num === null || typeof num === 'undefined') {
+    return '0';
+  }
   if (num >= 1000000) {
     return (num / 1000000).toFixed(1) + 'M';
   }
